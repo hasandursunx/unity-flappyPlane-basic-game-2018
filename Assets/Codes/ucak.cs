f346a1358ef8267fe745d4b993cc45bd7b57ec86
hasandursunx <hasandursunx@gmail.com>
2023-10-27 06:29:56 +0300

    first commit

---
+++ Assets/Codes/ucak.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ucak : MonoBehaviour
+{
+    public float hiz, yukselisHizi,mesafe,skormesafe;
+    public Transform baslangicNoktasi;
+    public Text mesafeYazi,oyunSonuMesafeYazi,skorMesafeYazi;
+    public GameObject baslangicPanel, oyunsonuPanel, oyunPanel;
+    void Start()
+    {
+        baslangicPanel.SetActive(true);
+        oyunPanel.SetActive(false);
+        oyunsonuPanel.SetActive(false);
+        hiz = 0;
+        yukselisHizi = 0;
+        GetComponent<Rigidbody2D>().gravityScale = 0;
+        
+    }
+
+    void Update()
+
+    {
+        mesafeYazi.text = (int)mesafe + "M";
+
+        // ucagin bulunduğu konum ile kendi konumu arasındaki mesafeyi ölçme
+        mesafe = Vector2.Distance(baslangicNoktasi.position, transform.position);
+    
+        transform.Translate(hiz*Time.deltaTime,0,0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * yukselisHizi);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D nesne)
+    {
+        if (nesne.gameObject.tag == "Gecis")
+        {
+            nesne.gameObject.transform.root.gameObject.GetComponent<yol> ().aktif = true;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D nesne)
+    {
+        if (nesne.gameObject.tag == "Engel")
+        {
+            Time.timeScale = 0;
+            OyunSonu();
+        }
+    }
+
+    public void Butonlar(int i)
+    {
+        if(i == 0)
+        {
+            hiz = 5;
+            yukselisHizi = 300;
+            GetComponent<Rigidbody2D>().gravityScale = 1;
+            oyunPanel.SetActive(true);
+            baslangicPanel.SetActive(false);
+        }
+    }
+
+    public void OyunSonu()
+    {
+        oyunPanel.SetActive(false);
+        oyunsonuPanel.SetActive(true);
+        oyunSonuMesafeYazi.text = "Mesafe : " + (int)mesafe + " M ";
+
+        skormesafe = PlayerPrefs.GetFloat("SkorMesafemiz");
+        if (skormesafe > mesafe)
+        {
+            skorMesafeYazi.text = "Skor Mesafe : " + (int)skormesafe + "M";
+        }
+        else
+        {
+            skormesafe = mesafe;
+            PlayerPrefs.SetFloat("SkorMesafemiz", skormesafe);
+            skorMesafeYazi.text = "Skor Mesafe : " + (int)skormesafe + "M";
+
+        }
+    }
+}
---
+++ Assets/Codes/yol.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yol : MonoBehaviour
+{
+    public bool aktif;
+    public bool puanmi;
+    public GameObject[] objeler; // objeler dizisi
+    void Start()
+    {
+
+        float altsayi = Random.Range(0, 4); // 0 ile 2 arası rastgele sayı oluşturmak
+        float ustsayi = Random.Range(0, 4); // 0 ile 2 arası rastgele sayı oluşturmak
+
+        // alt dikenin rastgele sayılarla açık kapalı olmasını belirlemek
+        if (altsayi == 1)
+        {
+            objeler[1].SetActive(false);
+        }
+        else
+        {
+            objeler[1].SetActive(true);
+        }
+
+        // üst dikenin rastgele sayılarla açık kapalı olmasını belirlemek
+        if (ustsayi == 1)
+        {
+            objeler[2].SetActive(false);
+        }
+        else
+        {
+            objeler[2].SetActive(true);
+        }
+
+
+        if (altsayi == 1 && ustsayi == 1)
+        {
+            objeler[3].transform.localPosition = new Vector2(0, Random.Range(-2.5f, 2.5f));
+        }
+        else if (altsayi == 1 && ustsayi != 1)
+        {
+            objeler[3].transform.localPosition = new Vector2(0, Random.Range(-2.5f, 0));
+        }
+        else if (altsayi != 1 && ustsayi == 1)
+        {
+            objeler[3].transform.localPosition = new Vector2(0, Random.Range(0,2.5f));
+        }
+        else if (altsayi != 1 && ustsayi != 1)
+        {
+            objeler[3].transform.localPosition = new Vector2(0, 0);
+        }
+
+
+        // diken sarkıkların -3.5 ile 3.5 arasında oluşmasını sağlıyor
+        objeler[0].transform.localPosition = new Vector2(Random.Range(-2.5f, 2.5f), 0);
+
+      
+
+        // yıldız oluşturur
+        objeler[3].SetActive(true); 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (aktif == true)
+        {
+            if (puanmi == true)
+            {
+                objeler[3].SetActive(false); // yıldızları siler
+            }
+            Invoke("YoluTasi",3); // yoltasi komutunu 3 saniye sonra oluşturur
+            aktif = false;
+        }
+
+      
+        
+    }
+
+    void YoluTasi()
+    {
+        float altsayi = Random.Range(0, 4); // 0 ile 2 arası rastgele sayı oluşturmak
+        float ustsayi = Random.Range(0, 4); // 0 ile 2 arası rastgele sayı oluşturmak
+
+        // alt dikenin rastgele sayılarla açık kapalı olmasını belirlemek
+        if (altsayi == 1)
+        {
+            objeler[1].SetActive (false);
+        }
+        else
+        {
+            objeler[1].SetActive(true);
+        }
+
+        // üst dikenin rastgele sayılarla açık kapalı olmasını belirlemek
+        if (ustsayi == 1 )
+        {
+            objeler[2].SetActive(false);
+        }
+        else
+        {
+            objeler[2].SetActive(true);
+        }
+
+        if (altsayi == 1 && ustsayi == 1)
+        {
+            objeler[3].transform.localPosition = new Vector2(0, Random.Range(-2.5f, 2.5f));
+        }
+        else if (altsayi == 1 && ustsayi != 1)
+        {
+            objeler[3].transform.localPosition = new Vector2(0, Random.Range(-2.5f, 0));
+        }
+        else if (altsayi != 1 && ustsayi == 1)
+        {
+            objeler[3].transform.localPosition = new Vector2(0, Random.Range(0, 2.5f));
+        }
+        else if (altsayi != 1 && ustsayi != 1)
+        {
+            objeler[3].transform.localPosition = new Vector2(0, 0);
+        }
+
+
+      
+        objeler[0].transform.localPosition = new Vector2(Random.Range(2.5f, -2.5f), 0);
+        
+        // pozisyonu her 3 sn bir değişsin
+        transform.position = transform.position + new Vector3(40.32f, 0, 0);
+
+        // yıldız oluşturur
+        objeler[3].SetActive(true); 
+    }
+}
